ece3ef6d1ed3b0f74c60eb43628f5b3eba8ae8f1
Sandip Jadhav <154830047+sandip-jadhao@users.noreply.github.com>
2025-11-13 10:53:17 +0530

    Add database port configuration

--- backend/db.js
+++ backend/db.js
@@ -8,6 +8,7 @@ let conn=mysql.createConnection({
     user:process.env.DB_USERNAME,
     password:process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
+    port:process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
